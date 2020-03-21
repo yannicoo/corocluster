3718f52370aeabe77f458639e71977da1d5ef6c1
yannicoo <yannic@Yannics-Surface.localdomain>
2020-03-22 00:20:32 +0100

    Working fitting routine for single examples

--- import_as_pd.py
+++ import_as_pd.py
@@ -9,6 +9,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime
+# from datetime import timedelta
+from scipy.optimize import curve_fit
+
+
+# exponential functino for the fit
+def func(x, a, b, c):
+    return a * np.exp(b*x) + c
 
 
 # import csv file as pandas DataFrame
@@ -22,18 +29,31 @@ df = pd.read_csv("data.csv")
 df = df[df['Landkreis'] == "LK Heinsberg"]
 # plt.plot(arr[:,5])
 # arr = arr.sort(axis=8)
-df['Meldedatum'].map(lambda x: datetime.strptime(x, '%Y-%m-%dT%H:%M:%S.%f%z').date())
+df['Meldedatum'].map(
+    lambda x: datetime.strptime(x, '%Y-%m-%dT%H:%M:%S.%f%z').date())
 df.sort_values("Meldedatum", inplace=True)
+df_sum = df.groupby(["Meldedatum"]).sum()
 
-plt.plot(df["Meldedatum"], df['AnzahlFall'])
+# array with same length as datapoints
 
-# a = []
-# i = 0
-# while(arr[i, 2]=="SK Flensburg"):
-#     a.append(arr[i, :])
-#     i+=1
+# df_sum = df.sum("Meldedatum")
 
-# a = np.array(a)
+arr = np.arange(len(df_sum))
+df_sum = df_sum.cumsum()
+
+
+# the fit is performed here
+popt, pcov = curve_fit(func, arr, df_sum['AnzahlFall'])
+
+plt.figure()
+plt.grid()
+plt.xlabel("Tag seit Erfassung")
+plt.ylabel("Anzahl der Infizierten")
+plt.plot(arr, df_sum['AnzahlFall'], "o", label="data")
+plt.plot(arr, func(arr, *popt), label="fit")
+plt.legend()
+plt.show()
+
 
 # def fermi(x):
 #     return 1 / (np.exp(-x)+1)
@@ -44,4 +64,3 @@ plt.plot(df["Meldedatum"], df['AnzahlFall'])
 # plt.grid()
 # plt.plot(x + 10, fermi(x))
 # plt.show()
-
